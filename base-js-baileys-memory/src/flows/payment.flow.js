0bc490f04c5b5a3b53e36f319ae6d62bccd9eead
yohanderjose2002 <yohanderjose2002@gmail.com>
2025-08-10 22:50:05 +0000

    update

--- base-js-baileys-memory/src/flows/payment.flow.js
+++ base-js-baileys-memory/src/flows/payment.flow.js
@@ -4,7 +4,6 @@ import fs from 'fs';
 import path from 'path';
 
 const MEDIA_PATH = path.join(process.cwd(), 'media');
-const pool = mysql.createPool(dbConfig);
 
 if (!fs.existsSync(MEDIA_PATH)) {
     fs.mkdirSync(MEDIA_PATH, { recursive: true });
